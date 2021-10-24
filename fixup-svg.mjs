effc19d0497ade6995643600fc5e6185555946df
Erin <erin20913@gmail.com>
2021-10-24 17:02:17 -0400

    Clean up SVG code somewhat

--- fixup-svg.mjs
+++ fixup-svg.mjs
@@ -1,48 +1,57 @@
 import * as fs from 'fs';
 import * as childProcess from 'child_process';
 
-let overlaySVG = fs.readFileSync(process.argv[2], 'utf-8');
+/**
+ * Given the contents of an SVG document, rewrites SVG <font> elements in the
+ * document as CSS @font-face rules with embedded base64 font data.
+ */
+function convertSVGFonts (svg) {
+	let preamble = svg.match(/^[\s\S]*?<svg[^>]*?>/)[0];
 
-let preamble = overlaySVG.match(/^[\s\S]*?<svg[^>]*?>/)[0];
+	// Separate out <font>s and process them individually
+	let fonts = svg.match(/<font[\s\S]*?<\/font>/g);
+	for (let fontSource of fonts) {
+		// Get the name of the font family so we know what to call it
+		let fontFamily = fontSource.match(/font-family="([^"]*)"/)[1];
 
-// Separate out <font>s and process them individually
-let fonts = overlaySVG.match(/<font[\s\S]*?<\/font>/g);
-for (let fontSource of fonts) {
-	// Get the name of the font family so we know what to call it
-	let fontFamily = fontSource.match(/font-family="([^"]*)"/)[1];
+		// Add the parts of the glyph outlines that Illustrator didn't generate, and
+		// wrap the whole thing in <svg> so FontForge recognizes it
+		const fixedFontSource = `${preamble}${fontSource.replace(/M(-?\d+,-?\d+)([^z]*?)(?=M|")/g, 'M$1$2L$1')}</svg>`;
 
-	// Add the parts of the glyph outlines that Illustrator didn't generate, and
-	// wrap the whole thing in <svg> so FontForge recognizes it
-	const fixedFontSource = `${preamble}${fontSource.replace(/M(-?\d+,-?\d+)([^z]*?)(?=M|")/g, 'M$1$2L$1')}</svg>`;
+		// Write the output to a temporary file because fontforge only opens files
+		fs.writeFileSync(`out-${fontFamily}.svg`, fixedFontSource, {
+			encoding: 'utf-8'
+		});
 
-	// Write the output to a temporary file because fontforge only opens files
-	fs.writeFileSync(`out-${fontFamily}.svg`, fixedFontSource, {
-		encoding: 'utf-8'
-	});
+		// Do some FontForge magic to convert the font from SVG to TTF
+		// this is incredibly unsafe lmao
+		// tried this at first with WOFF2 but ended up getting browser errors, TTF seems more stable
+		childProcess.execSync(`"C:\\Program Files (x86)\\FontForgeBuilds\\bin\\fontforge.exe" -c "open('out-${fontFamily}.svg').generate('out-${fontFamily}.ttf')"`, {stdio: 'pipe'});
 
-	// Do some FontForge magic to convert the font from SVG to TTF
-	// this is incredibly unsafe lmao
-	// tried this at first with WOFF2 but ended up getting browser errors, TTF seems more stable
-	childProcess.execSync(`"C:\\Program Files (x86)\\FontForgeBuilds\\bin\\fontforge.exe" -c "open('out-${fontFamily}.svg').generate('out-${fontFamily}.ttf')"`, {stdio: 'pipe'});
+		// Read the TTF file, shove it in a data: URI, wrap it in a @font-face
+		const woffSourceBuffer = fs.readFileSync(`out-${fontFamily}.ttf`);
+		let fontDataURI = `data:font/ttf;base64,${woffSourceBuffer.toString('base64')}`;
+		let fontFaceCSS = `
+			@font-face {
+				font-family: "${fontFamily}";
+				src: url(${fontDataURI});
+			}
+		`;
 
-	// Read the TTF file, shove it in a data: URI, wrap it in a @font-face
-	const woffSourceBuffer = fs.readFileSync(`out-${fontFamily}.ttf`);
-	let fontDataURI = `data:font/ttf;base64,${woffSourceBuffer.toString('base64')}`;
-	let fontFaceCSS = `
-		@font-face {
-			font-family: "${fontFamily}";
-			src: url(${fontDataURI});
-		}
-	`;
+		// Replace the original font data with a <style> that implements the font
+		svg = svg.replace(fontSource, `<style>${fontFaceCSS}</style>`);
+	}
 
-	// Replace the original font data with a <style> that implements the font
-	overlaySVG = overlaySVG.replace(fontSource, `<style>${fontFaceCSS}</style>`);
+	return svg;
 }
 
-// Wrap the SVG in an HTML document
-let overlayHTML = fs.readFileSync('template.html', 'utf-8')
-	.replace('OVERLAY_SVG_GOES_HERE', overlaySVG);
+const outputHTMLTemplate = fs.readFileSync('template.html', 'utf-8');
 
+// Get input
+let overlaySVG = fs.readFileSync(process.argv[2], 'utf-8');
+// Convert fonts and insert HTML wrapper
+let overlayHTML = outputHTMLTemplate.replace('OVERLAY_SVG_GOES_HERE', convertSVGFonts(overlaySVG));
+// Write output
 fs.writeFileSync(process.argv[3], overlayHTML, 'utf-8');
 
 console.log(`Written HTML to ${process.argv[3]}`);
